82521481bde19d13c062091c55806bb47e87fb35
소희철 <thrrkthrrk@gmail.com>
2019-05-02 21:53:21 +0900

    Initialize

---
+++ src/main/kotlin/com/example/studyspringreactiveweb/StudySpringReactiveWebApplication.kt
@@ -0,0 +1,11 @@
+package com.example.studyspringreactiveweb
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StudySpringReactiveWebApplication
+
+fun main(args: Array<String>) {
+    runApplication<StudySpringReactiveWebApplication>(*args)
+}
